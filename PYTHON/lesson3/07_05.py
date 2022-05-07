72ccee7c6b1ef73853ef486bf7c783a48ac6ddc8
alex101410 <98638552+alex101410@users.noreply.github.com>
2022-05-07 11:36:15 +0300

    1

--- PYTHON/lesson3/07_05.py
+++ PYTHON/lesson3/07_05.py
@@ -24,7 +24,7 @@ if d > 0:
     x2 = (-b - d ** 0.5) / (2 * a)
     print("x1 = ", x1, "x2 = ", x2)
 elif d == 0:
-    x1 = (-b + d ** 0.5) / (2 * a)
+    x1 = -b / 2 * a
     print("x1 = ", x1)
 else:
     print("no roots")
