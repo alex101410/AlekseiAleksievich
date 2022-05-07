aadac9cb30cf52b7e8cc876c8f701d93f430ebee
alex101410 <98638552+alex101410@users.noreply.github.com>
2022-05-07 11:32:29 +0300

    07_05

---
+++ PYTHON/lesson3/07_05.py
@@ -0,0 +1,30 @@
+#x = input("please enter")
+#print(x)
+
+#x = 3 / True 
+#print(x)
+
+#x = 3 - 1
+#if x == 1:
+#    print(x)
+#elif x == 2:
+#    print(2)
+#else:
+#    print(0)
+
+
+
+a = int(input("please enter a")) 
+b = int(input("please enter b"))
+c = int(input("please enter c"))
+d = b ** 2 - 4 * a * c
+print("d = ", d)
+if d > 0:
+    x1 = (-b + d ** 0.5) / (2 * a)
+    x2 = (-b - d ** 0.5) / (2 * a)
+    print("x1 = ", x1, "x2 = ", x2)
+elif d == 0:
+    x1 = (-b + d ** 0.5) / (2 * a)
+    print("x1 = ", x1)
+else:
+    print("no roots")
