02809398c3313816e1e95e840c8824a523c982b8
alex101410 <98638552+alex101410@users.noreply.github.com>
2022-05-08 10:38:56 +0300

    lesson_08_05

---
+++ PYTHON/lesson4/08_05.py
@@ -0,0 +1,9 @@
+s = "my very long string"
+print(len(s))
+print(s[1:7])
+print("my " in s)
+print(s.find("y"), s[s.find("y")])
+print(s.upper(), s)
+#s = s.replace(" ", "_")
+print(s)
+print(s.split())
