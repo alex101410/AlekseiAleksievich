76dc5e71b30e9ad7ec3c84fd3ff3047524d8201d
alex101410 <98638552+alex101410@users.noreply.github.com>
2022-06-08 22:47:23 +0300

    08_06

--- PYTHON/homework/homework_4.py
+++ PYTHON/homework/homework_4.py
@@ -10,27 +10,31 @@ def scheck_str(string):
 scheck_str("The quick Brown Fox")
 
 
-def is_prime (number_to_chek):
+def is_prime(number_to_chek):
     for x in range(2, number_to_chek):
         if (number_to_chek % x == 0):
             return False
     return True
-
 print(is_prime(787))
 print(is_prime(777))
 
 
-#3. Реализовать функцию get_ranges которая получает на вход непустой список неповторяющихся целых чисел, 
-#отсортированных по возрастанию, и которая этот список “сворачивает”.
-
-#get_ranges([0, 1, 2, 3, 4, 7, 8, 10])  ->  "0-4, 7-8, 10"
-#get_ranges([4,7,10])  -> "4, 7, 10"
-#get_ranges([2, 3, 8, 9])  -> "2-3, 8-9"
-
-
-#def get_ranges(list):
-#    for j[i] in list:
-#        if j[i+1] == j+1:
-#            list.append(j)
-#    print(list)
-#get_ranges([0, 1, 2, 3, 4, 7, 8, 10])
+def get_ranges(sorted_lst):
+        i = 0
+        collapsed_list = [[sorted_lst[0], sorted_lst[0]]]
+        for j in sorted_lst:
+            if collapsed_list[i][1] in (j, j-1):
+                collapsed_list[i][1] = j
+            else:
+                collapsed_list.append([j, j])
+                i += 1
+        collapsed_list_str = ""
+        for k in collapsed_list:
+            if k[0] != k[1]:
+                collapsed_list_str = f"{collapsed_list_str},{k[0]}-{k[1]}"
+            else:
+                collapsed_list_str = f"{collapsed_list_str},{k[0]}"
+        return collapsed_list_str[1:]
+print(f"\"{get_ranges([0, 1, 2, 3, 4, 7, 8, 10])}\"")
+print(f"\"{get_ranges([4,7,10])}\"")
+print(f"\"{get_ranges([2, 3, 8, 9])}\"")
