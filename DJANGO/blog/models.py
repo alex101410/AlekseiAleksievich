64593ba8a5297589e5bb22be9fab806c23a352e5
alex101410 <98638552+alex101410@users.noreply.github.com>
2022-07-15 18:08:43 +0300

    15_07

--- DJANGO/blog/models.py
+++ DJANGO/blog/models.py
@@ -1,3 +1,21 @@
+import email
+from email.mime import image
+from turtle import title
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(blank=False, max_length=100)
+    email = models.EmailField(blank=False,primary_key=True)
+
+class Post(models.Model):
+
+    POST_TYPE = [('c', 'copyright'),('a', 'author')]
+
+    title = models.CharField(blank=False, max_length=200)
+    subtitle = models.CharField(max_length=400)
+    content = models.TextField(blank=False)
+    issued = models.DateField()
+    post_type = models.CharField(max_length=1, blank=False, choices=POST_TYPE)
+    image = models.ImageField(upload_to='uploads')
+
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
