7d4adccd9adea5c4ed1806a190863a54b51194ee
alex101410 <98638552+alex101410@users.noreply.github.com>
2022-07-24 14:57:10 +0300

    24.07

--- DJANGO/mysite/urls.py
+++ DJANGO/mysite/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from blog import views as blog_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blog_views.home)
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ DJANGO/temporary/parse.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup as BS
+import csv
+
+HEADERS = {
+    'Host': 'www.ml-auto.by',
+    'Referer': 'https://www.ml-auto.by/search/preload/?article=K2056c3',
+    'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest'
+}
+
+
+url = 'https://www.ml-auto.by/search/number/?article=K2056C3&brand=FENOX&ajax=true'
+r = requests.get(url, headers=HEADERS)
+soup = BS(r.text, 'html.parser')
+
+
+catalog = []
+i = 0
+
+for k in soup:
+    catalog.append({
+            'brend': soup.find('a', class_='fancy_inline droppeda danger-hover-link brand-search').text.strip(),
+            'article': soup.find('a', style="font-size: 14px").text.strip(),
+            'quantity': soup.find('div', class_='col-4 col-sm-2 col-lg-1 text-center m-auto').text.strip(),
+            'cost': soup.find('span', class_='old_PriceLevel').text.strip()
+            })
+    i =+ 1
+print(catalog)
+with open('parse.csv', 'w',  encoding='utf8', newline='') as file:
+    writer = csv.writer(file, delimiter=',')
+    writer.writerow(['Бренд', 'Артикул', 'Количество', 'Цена'])
+    for i in catalog:
+        writer.writerow([i['brend'], i['article'], i['quantity'], i['cost']])
